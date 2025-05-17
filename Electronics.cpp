5bd151bfa003d355093cce8f3b1314f3b1bd7f0e
TekGio <orellana.gio05@gmail.com>
2025-05-17 13:34:44 -0700

    Update Electronics.cpp

--- Electronics.cpp
+++ Electronics.cpp
@@ -1,9 +1,46 @@
-//
-// Created by Jawad Khadra on 5/14/25.
-//
-
 #include "Electronics.h"
+#include <iostream>
 
-using namespace std;
+// Constructor
+Electronics::Electronics(const std::string& name,
+                         double price,
+                         const std::string& brand,
+                         const std::string& model,
+                         int warrantyMonths)
+  : Product(name, price)
+  , brand(brand)
+  , model(model)
+  , warrantyMonths(warrantyMonths)
+{}
 
-// TODO: Add implementation of Electronics class.
+// Type tag
+std::string Electronics::getType() const {
+    return "Electronics";
+}
+
+// Print all details
+void Electronics::display(std::ostream& os) const {
+    os  << "[Electronics] "
+        << brand
+        << " "
+        << name
+        << " (Model: "
+        << model
+        << ", "
+        << warrantyMonths
+        << "-month warranty) - $"
+        << price;
+}
+
+// Accessors
+const std::string& Electronics::getBrand() const {
+    return brand;
+}
+
+const std::string& Electronics::getModel() const {
+    return model;
+}
+
+int Electronics::getWarrantyMonths() const {
+    return warrantyMonths;
+}
