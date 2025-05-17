3fb86008280f10626c0e6949ec1d0225b00b958a
TekGio <orellana.gio05@gmail.com>
2025-05-17 13:35:14 -0700

    Update Clothing.h

--- Clothing.h
+++ Clothing.h
@@ -1,18 +1,28 @@
-//
-// Created by Jawad Khadra on 5/14/25.
-//
-
 #ifndef CLOTHING_H
 #define CLOTHING_H
-
-// Jawad Khadra
 #include "Product.h"
-#include "project.h"
+#include <string>
 
-// Jawad Khadra
 class Clothing : public Product {
-protected:
 public:
-};
+    Clothing(const std::string& name,
+             double price,
+             const std::string& size,
+             const std::string& material,
+             const std::string& brand);
 
+    // Product interface
+    std::string getType() const override;
+    void display(std::ostream& os) const override;
+
+    // Accessors
+    const std::string& getSize() const;
+    const std::string& getMaterial() const;
+    const std::string& getBrand() const;
+
+private:
+    std::string size;
+    std::string material;
+    std::string brand;
+};
 #endif //CLOTHING_H
