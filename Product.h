5f0c101d50230c1d4087843c5271a550f906a9fa
TekGio <orellana.gio05@gmail.com>
2025-05-17 13:31:34 -0700

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,21 +1,24 @@
-//
-// Created by Jawad Khadra on 5/14/25.
-//
-
 #ifndef PRODUCT_H
 #define PRODUCT_H
+#include <iostream>
+#include <string>
 
-// Jawad Khadra
-#include "project.h"
-
-using namespace std;
-
-// Jawad Khadra
 class Product {
-protected:
-
 public:
+    Product(const std::string& name, double price);
+    virtual ~Product() = default;
 
+    virtual std::string getType() const = 0;
+    virtual void display(std::ostream& os) const = 0;
+
+    std::string getName() const;
+    double getPrice() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Product& product);
+    bool operator==(const Product& other) const;
+
+protected:
+    std::string name;
+    double price;
 };
-
 #endif //PRODUCT_H
