284eaafb0f44590a40190ed0b43ef14a5c09f88f
TekGio <orellana.gio05@gmail.com>
2025-05-17 13:34:24 -0700

    Update Electronics.h

--- Electronics.h
+++ Electronics.h
@@ -1,20 +1,30 @@
-//
-// Created by Jawad Khadra on 5/14/25.
-//
-
 #ifndef ELECTRONICS_H
 #define ELECTRONICS_H
-
-// Jawad Khadra
 #include "Product.h"
 #include "project.h"
+#include <string>
 
-using namespace std;
-
-// Jawad Khadra
 class Electronics : public Product {
-protected:
 public:
+    Electronics(const std::string& name,
+                double price,
+                const std::string& brand,
+                const std::string& model,
+                int warrantyMonths);
+
+    // Product interface
+    std::string getType() const override;
+    void display(std::ostream& os) const override;
+
+    // Accessors
+    const std::string& getBrand() const;
+    const std::string& getModel() const;
+    int getWarrantyMonths() const;
+
+private:
+    std::string brand;
+    std::string model;
+    int warrantyMonths;
 };
 
 #endif //ELECTRONICS_H
