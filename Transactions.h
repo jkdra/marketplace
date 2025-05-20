3a4d27b232eb09e247a62efbbbc7f9624ec91c07
TekGio <orellana.gio05@gmail.com>
2025-05-19 18:53:18 -0700

    Update Transactions.h

--- Transactions.h
+++ Transactions.h
@@ -1,5 +1,6 @@
 #ifndef TRANSACTIONS_H
 #define TRANSACTIONS_H
+
 #include <string>
 #include <memory>
 #include "User.h"
@@ -11,6 +12,9 @@ public:
                 std::shared_ptr<User> seller,
                 std::shared_ptr<Product> product);
 
+    // ID access
+    const std::string& getId() const;
+
     std::shared_ptr<User> getBuyer() const;
     std::shared_ptr<User> getSeller() const;
     std::shared_ptr<Product> getProduct() const;
@@ -19,11 +23,16 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const Transaction& transaction);
 
 private:
+    std::string id;
     std::shared_ptr<User> buyer;
     std::shared_ptr<User> seller;
     std::shared_ptr<Product> product;
     std::string timestamp;
 
     void setTimestamp();
+
+    static std::size_t nextTxId;
+    static std::string makeTxId();
 };
+
 #endif //TRANSACTIONS_H
