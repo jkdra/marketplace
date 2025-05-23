f0b82130b6f9d900df54e6b01a5d9d2352f1173a
jawadkhadra <jawad.alkhadra@gmail.com>
2025-05-23 06:35:39 -0700

    Added more cases to Exceptions.h according to requirements

--- Exceptions.h
+++ Exceptions.h
@@ -12,4 +12,22 @@ class CacheException : public std::runtime_error {
 	CacheException() : std::runtime_error("invalid cache") {}
 };
 
+// Jawad Khadra
+class InsufficientFundsException : public std::runtime_error {
+public:
+InsufficientFundsException() : std::runtime_error("Insufficient funds") {}
+}
+
+// Jawad Khadra
+class ProductNotFoundException : public std::runtime_error {
+public:
+ProductNotFoundException() : std::runtime_error("Product not found") {}
+}
+
+// Jawad Khadra
+class UnauthorizedActionException : public std::runtime_error {
+public:
+UnauthorizedActionException() : std::runtime_error("Unauthorized action") {}
+}
+
 #endif // EXCEPTIONS_H
